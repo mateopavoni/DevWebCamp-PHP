48e3ecec8290abc5c8f76083befa30158ce8fc14
Mateo Pavoni <mateopavoni905@gmail.com>
2025-12-13 22:33:49 -0300

    feat: cree vista de login

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,1 +1,21 @@
-<h1><?php echo $titulo; ?></h1>
+<main class="auth">
+    <h2 class="auth__heading"><?php echo $titulo ?></h2>
+    <p class="auth__texto">Inicia sesión en DevWebCamp</p>
+
+    <form class="formulario">
+        <div class="formulario__campo">
+            <label class="formulario__label" for="email">Email</label>
+            <input class="formulario__input" type="email" id="email" placeholder="Tu email" name="email">
+        </div>
+        <div class="formulario__campo">
+            <label class="formulario__label" for="password">Password</label>
+            <input class="formulario__input" type="password" id="password" placeholder="Tu password" name="password">
+        </div>
+        <input class="formulario__submit" type="submit" value="Iniciar Sesión">
+    </form>
+
+    <div class="acciones">
+        <a class="acciones__enlace" href="/registro">¿Aún no tienes una cuenta? Regístrate</a>
+        <a class="acciones__enlace" href="/olvide">¿Olvidaste tu password?</a>
+    </div>
+</main>
