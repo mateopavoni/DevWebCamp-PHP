7aeeb2bbc07ddb4f23660e59f72f9938c5a31ba6
Mateo Pavoni <mateopavoni905@gmail.com>
2026-01-16 01:16:31 -0300

    feat: agregue estilos y logica para los tags de las areas de experiencia

--- views/admin/ponentes/formulario.php
+++ views/admin/ponentes/formulario.php
@@ -1,93 +1,108 @@
 <fieldset class="formulario__fieldset">
     <legend class="formulario__legend">Información Personal</legend>
+
     <div class="formulario__campo">
-        <label class="formulario__label" for="nombre">Nombre</label>
-        <input 
-            class="formulario__input" 
-            type="text" 
-            id="nombre" 
-            name="nombre" 
-            placeholder="Nombre del ponente"
-            value="<?php echo s($ponente?->nombre ?? ''); ?>"
-        >
-    </div>
-    <div class="formulario__campo">
-        <label class="formulario__label" for="nombre">Apellido</label>
-        <input 
-            class="formulario__input" 
-            type="text" 
-            id="apellido" 
-            name="apellido" 
-            placeholder="Apellido del ponente"
-            value="<?php echo s($ponente?->nombre ?? ''); ?>"
+        <label for="nombre" class="formulario__label">Nombre</label>
+        <input
+            type="text"
+            class="formulario__input"
+            id="nombre"
+            name="nombre"
+            placeholder="Nombre Ponente"
+            value="<?php echo $ponente->nombre ?? ''; ?>"
         >
     </div>
 
     <div class="formulario__campo">
-        <label class="formulario__label" for="nombre">País</label>
-        <input 
-            class="formulario__input" 
-            type="text" 
-            id="pais" 
-            name="pais" 
-            placeholder="País del ponente"
-            value="<?php echo s($ponente?->pais ?? ''); ?>"
+        <label for="apellido" class="formulario__label">Apellido</label>
+        <input
+            type="text"
+            class="formulario__input"
+            id="apellido"
+            name="apellido"
+            placeholder="Apellido Ponente"
+            value="<?php echo $ponente->apellido ?? ''; ?>"
         >
     </div>
 
     <div class="formulario__campo">
-        <label class="formulario__label" for="nombre">Ciudad</label>
-        <input 
-            class="formulario__input" 
-            type="text" 
-            id="ciudad" 
-            name="ciudad" 
-            placeholder="Ciudad del ponente"
-            value="<?php echo s($ponente?->ciudad ?? ''); ?>"
+        <label for="ciudad" class="formulario__label">Ciudad</label>
+        <input
+            type="text"
+            class="formulario__input"
+            id="ciudad"
+            name="ciudad"
+            placeholder="Ciudad Ponente"
+            value="<?php echo $ponente->ciudad ?? ''; ?>"
         >
     </div>
 
     <div class="formulario__campo">
-        <label class="formulario__label" for="nombre">Imagen</label>
-        <input 
-            class="formulario__input formulario__input--file" 
-            type="file" 
-            id="imagen" 
-            name="imagen" 
+        <label for="pais" class="formulario__label">País</label>
+        <input
+            type="text"
+            class="formulario__input"
+            id="pais"
+            name="pais"
+            placeholder="País Ponente"
+            value="<?php echo $ponente->pais ?? ''; ?>"
         >
     </div>
+    <div class="formulario__campo">
+        <label for="imagen" class="formulario__label">Imagen</label>
+        <input
+            type="file"
+            class="formulario__input formulario__input--file"
+            id="imagen"
+            name="imagen"
+        >
+    </div>
+
+    <?php if(isset($ponente->imagen_actual)) { ?>
+        <p class="formulario__texto">Imagen Actual:</p>
+        <div class="formulario__imagen">
+            <picture>
+                <source srcset="<?php echo $_ENV['HOST'] . '/img/speakers/' . $ponente->imagen; ?>.webp" type="image/webp">
+                <source srcset="<?php echo $_ENV['HOST'] . '/img/speakers/' . $ponente->imagen; ?>.png" type="image/png">
+                <img src="<?php echo $_ENV['HOST'] . '/img/speakers/' . $ponente->imagen; ?>.png" alt="Imagen Ponente">
+            </picture>
+        </div>
+
+    <?php } ?>
 </fieldset>
+
 
 <fieldset class="formulario__fieldset">
     <legend class="formulario__legend">Información Extra</legend>
+
     <div class="formulario__campo">
-        <label class="formulario__label" for="nombre">Áreas de experiencia (separadas por coma)</label>
-        <input 
-            class="formulario__input" 
-            type="text" 
-            id="tags_input" 
-            placeholder="Node, Vue, React, PHP, Laravel, etc"
+        <label for="tags_input" class="formulario__label">Áreas de Experiencia (separadas por coma)</label>
+        <input
+            type="text"
+            class="formulario__input"
+            id="tags_input"
+            placeholder="Ej. Node.js, PHP, CSS, Laravel, UX / UI"
         >
 
         <div id="tags" class="formulario__listado"></div>
-        <input type="hidden" name="tags" value="<?php echo s($ponente?->tags ?? ''); ?>">
+        <input type="hidden" name="tags" value="<?php echo $ponente->tags ?? ''; ?>"> 
     </div>
 </fieldset>
 
 <fieldset class="formulario__fieldset">
     <legend class="formulario__legend">Redes Sociales</legend>
+
     <div class="formulario__campo">
         <div class="formulario__contenedor-icono">
             <div class="formulario__icono">
-                <i class="fa-brands fa-facebook-f"></i>
+                <i class="fa-brands fa-facebook"></i>
             </div>
-
-            <input 
-                class="formulario__input--sociales" 
-                type="text" 
+            <input
+                type="text"
+                class="formulario__input--sociales"
                 name="redes[facebook]"
                 placeholder="Facebook"
-                value="<?php echo s($ponente?->redes['facebook'] ?? ''); ?>"
+                value="<?php echo $redes->facebook ?? ''; ?>"
             >
         </div>
     </div>
@@ -97,29 +112,12 @@
             <div class="formulario__icono">
                 <i class="fa-brands fa-twitter"></i>
             </div>
-
-            <input 
-                class="formulario__input--sociales" 
-                type="text" 
+            <input
+                type="text"
+                class="formulario__input--sociales"
                 name="redes[twitter]"
                 placeholder="Twitter"
-                value="<?php echo s($ponente?->redes['twitter'] ?? ''); ?>"
-            >
-        </div>
-    </div>
-
-    <div class="formulario__campo">
-        <div class="formulario__contenedor-icono">
-            <div class="formulario__icono">
-                <i class="fa-brands fa-instagram"></i>
-            </div>
-
-            <input 
-                class="formulario__input--sociales" 
-                type="text" 
-                name="redes[instagram]"
-                placeholder="Instagram"
-                value="<?php echo s($ponente?->redes['instagram'] ?? ''); ?>"
+                value="<?php echo $redes->twitter ?? ''; ?>"
             >
         </div>
     </div>
@@ -129,13 +127,27 @@
             <div class="formulario__icono">
                 <i class="fa-brands fa-youtube"></i>
             </div>
-
-            <input 
-                class="formulario__input--sociales" 
-                type="text" 
+            <input
+                type="text"
+                class="formulario__input--sociales"
                 name="redes[youtube]"
                 placeholder="YouTube"
-                value="<?php echo s($ponente?->redes['youtube'] ?? ''); ?>"
+                value="<?php echo $redes->youtube ?? ''; ?>"
+            >
+        </div>
+    </div>
+
+    <div class="formulario__campo">
+        <div class="formulario__contenedor-icono">
+            <div class="formulario__icono">
+                <i class="fa-brands fa-instagram"></i>
+            </div>
+            <input
+                type="text"
+                class="formulario__input--sociales"
+                name="redes[instagram]"
+                placeholder="Instagram"
+                value="<?php echo $redes->instagram ?? ''; ?>"
             >
         </div>
     </div>
@@ -145,13 +157,12 @@
             <div class="formulario__icono">
                 <i class="fa-brands fa-tiktok"></i>
             </div>
-
-            <input 
-                class="formulario__input--sociales" 
-                type="text" 
+            <input
+                type="text"
+                class="formulario__input--sociales"
                 name="redes[tiktok]"
                 placeholder="Tiktok"
-                value="<?php echo s($ponente?->redes['tiktok'] ?? ''); ?>"
+                value="<?php echo $redes->tiktok ?? ''; ?>"
             >
         </div>
     </div>
@@ -161,17 +172,14 @@
             <div class="formulario__icono">
                 <i class="fa-brands fa-github"></i>
             </div>
-
-            <input 
-                class="formulario__input--sociales" 
-                type="text" 
+            <input
+                type="text"
+                class="formulario__input--sociales"
                 name="redes[github]"
-                placeholder="Github"
-                value="<?php echo s($ponente?->redes['github'] ?? ''); ?>"
+                placeholder="GitHub"
+                value="<?php echo $redes->github ?? ''; ?>"
             >
         </div>
     </div>
 
-
 </fieldset>
-
