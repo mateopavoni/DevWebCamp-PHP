fa85bc205242dabbf3e92292a06d00f6c7c865d8
Mateo Pavoni <mateopavoni905@gmail.com>
2026-01-17 16:45:27 -0300

    feat: agregue el modelo de categoria para poder seleccioanr desde el formulario la categoria del evento

--- views/admin/eventos/formulario.php
+++ views/admin/eventos/formulario.php
@@ -1,24 +1,41 @@
 <fieldset class="formulario__fieldset">
-    <legend class="formulario__legend">Información del evento</legend>
+    <legend class="formulario__legend">Información Evento</legend>
+
     <div class="formulario__campo">
-        <label for="nombre" class="formulario__label">Nombre del evento</label>
+        <label for="nombre" class="formulario__label">Nombre Evento</label>
         <input
             type="text"
             class="formulario__input"
             id="nombre"
             name="nombre"
-            placeholder="Nombre del evento"
-            value="<?php echo $evento->nombre ?? ''; ?>"
+            placeholder="Nombre Evento"
+            value="<?php echo $evento->nombre; ?>"
         >
     </div>
+
     <div class="formulario__campo">
-        <label for="descripcion" class="formulario__label">Descripción del evento</label>
+        <label for="descripcion" class="formulario__label">Descripción</label>
         <textarea
-            class="formulario__input formulario__input--textarea"
+            class="formulario__input"
             id="descripcion"
             name="descripcion"
-            placeholder="Descripción del evento"
+            placeholder="Descripción Evento"
             rows="8"
-        ><?php echo $evento->descripcion ?? ''; ?></textarea>
+        ><?php echo $evento->descripcion; ?></textarea>
     </div>
+
+    <div class="formulario__campo">
+        <label for="categoria" class="formulario__label">Categoría o Tipo de Evento</label>
+        <select
+            class="formulario__select"
+            id="categoria"
+            name="categoria_id"
+        >
+            <option value="">- Seleccionar -</option>
+            <?php foreach($categorias as $categoria) { ?>
+                <option <?php echo ($evento->categoria_id === $categoria->id) ? 'selected' : '' ?> value="<?php echo $categoria->id; ?>"><?php echo $categoria->nombre; ?></option>
+            <?php } ?>
+        </select>
+    </div>    
+
 </fieldset>
