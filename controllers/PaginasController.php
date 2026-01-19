5d11a95aa7678bdb9bdae8ef0de3711fbb713854
Mateo Pavoni <mateopavoni905@gmail.com>
2026-01-19 18:37:06 -0300

    feat: agregue slide de eventos en el index.php, al igual que resumen de la cantidad de eventos, speakers, etc. Con sus respectivos estilos y logica

--- controllers/PaginasController.php
+++ controllers/PaginasController.php
@@ -11,8 +11,44 @@ use Model\Categoria;
 
 class PaginasController {
     public static function index(Router $router) {
+
+        $eventos = Evento::ordenar('hora_id', 'ASC');
+
+        $eventos_formateados = [];
+        foreach($eventos as $evento) {
+            $evento->categoria = Categoria::find($evento->categoria_id);
+            $evento->dia = Dia::find($evento->dia_id);
+            $evento->hora = Hora::find($evento->hora_id);
+            $evento->ponente = Ponente::find($evento->ponente_id);
+            
+            if($evento->dia_id === "1" && $evento->categoria_id === "1") {
+                $eventos_formateados['conferencias_v'][] = $evento;
+            }
+
+            if($evento->dia_id === "2" && $evento->categoria_id === "1") {
+                $eventos_formateados['conferencias_s'][] = $evento;
+            }
+
+            if($evento->dia_id === "1" && $evento->categoria_id === "2") {
+                $eventos_formateados['workshops_v'][] = $evento;
+            }
+
+            if($evento->dia_id === "2" && $evento->categoria_id === "2") {
+                $eventos_formateados['workshops_s'][] = $evento;
+            }
+        }
+
+        // Obtener el total de cada bloque
+        $ponentes_total = Ponente::total();
+        $conferencias_total = Evento::total('categoria_id', 1);
+        $workshops_total = Evento::total('categoria_id', 2);
+
         $router->render('paginas/index', [
-            'titulo' => 'Inicio'
+            'titulo' => 'Inicio',
+            'eventos' => $eventos_formateados,
+            'ponentes_total' => $ponentes_total,
+            'conferencias_total' => $conferencias_total,
+            'workshops_total' => $workshops_total
         ]);
     }
     public static function evento(Router $router) {
@@ -33,43 +69,36 @@ class PaginasController {
 
         $eventos = Evento::ordenar('hora_id', 'ASC');
 
-        $eventos_formateados = [
-            'conferencias_v' => [],
-            'conferencias_s' => [],
-            'workshops_v'    => [],
-            'workshops_s'    => []
-        ];
-
+        $eventos_formateados = [];
         foreach($eventos as $evento) {
             $evento->categoria = Categoria::find($evento->categoria_id);
             $evento->dia = Dia::find($evento->dia_id);
             $evento->hora = Hora::find($evento->hora_id);
             $evento->ponente = Ponente::find($evento->ponente_id);
-
-            if((int)$evento->dia_id === 1 && (int)$evento->categoria_id === 1) {
+            
+            if($evento->dia_id === "1" && $evento->categoria_id === "1") {
                 $eventos_formateados['conferencias_v'][] = $evento;
             }
 
-            if((int)$evento->dia_id === 2 && (int)$evento->categoria_id === 1) {
+            if($evento->dia_id === "2" && $evento->categoria_id === "1") {
                 $eventos_formateados['conferencias_s'][] = $evento;
             }
 
-            if((int)$evento->dia_id === 1 && (int)$evento->categoria_id === 2) {
+            if($evento->dia_id === "1" && $evento->categoria_id === "2") {
                 $eventos_formateados['workshops_v'][] = $evento;
             }
 
-            if((int)$evento->dia_id === 2 && (int)$evento->categoria_id === 2) {
+            if($evento->dia_id === "2" && $evento->categoria_id === "2") {
                 $eventos_formateados['workshops_s'][] = $evento;
             }
-
         }
 
+
         $router->render('paginas/conferencias', [
-            'titulo'  => 'Conferencias & Workshops',
+            'titulo' => 'Conferencias & Workshops',
             'eventos' => $eventos_formateados
         ]);
     }
-
 
     public static function error(Router $router) {
 
