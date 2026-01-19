53d52488fe3644875ce4857354340dd2ac7c1060
Mateo Pavoni <mateopavoni905@gmail.com>
2026-01-19 18:40:06 -0300

    feat: agregue estructura para mostrar todos los speakers en el index.php

--- controllers/PaginasController.php
+++ controllers/PaginasController.php
@@ -43,12 +43,16 @@ class PaginasController {
         $conferencias_total = Evento::total('categoria_id', 1);
         $workshops_total = Evento::total('categoria_id', 2);
 
+        // Obtener todos los ponentes
+        $ponentes = Ponente::all();
+
         $router->render('paginas/index', [
             'titulo' => 'Inicio',
             'eventos' => $eventos_formateados,
             'ponentes_total' => $ponentes_total,
             'conferencias_total' => $conferencias_total,
-            'workshops_total' => $workshops_total
+            'workshops_total' => $workshops_total,
+            'ponentes' => $ponentes
         ]);
     }
     public static function evento(Router $router) {
--- views/paginas/index.php
+++ views/paginas/index.php
@@ -26,3 +26,81 @@
     </div>
 </section>
 
+<section class="speakers">
+    <h2 class="speakers__heading">Speakers</h2>
+    <p class="speakers__descripcion">Conoce a nuestros expertos de DevWebCamp</p>
+
+    <div class="speakers__grid">
+        <?php foreach($ponentes as $ponente) { ?>
+            <div <?php aos_animacion(); ?> class="speaker">
+                <picture>
+                    <source srcset="img/speakers/<?php echo $ponente->imagen; ?>.webp" type="image/webp">
+                    <source srcset="img/speakers/<?php echo $ponente->imagen; ?>.png" type="image/png">
+                    <img class="speaker__imagen" loading="lazy" width="200" height="300" src="img/speakers/<?php echo $ponente->imagen; ?>.png" alt="Imagen Ponente">
+                </picture>
+
+                <div class="speaker__informacion">
+                    <h4 class="speaker__nombre">
+                        <?php echo $ponente->nombre . ' ' . $ponente->apellido; ?>
+                    </h4>
+
+                    <p class="speaker__ubicacion">
+                        <?php echo $ponente->ciudad . ', ' . $ponente->pais; ?>
+                    </p>
+
+                    <nav class="speaker-sociales">
+                        <?php
+                            $redes =  json_decode( $ponente->redes );
+                        ?>
+                        
+                        <?php if(!empty($redes->facebook)) { ?>
+                            <a class="speaker-sociales__enlace" rel="noopener noreferrer" target="_blank" href="<?php echo $redes->facebook; ?>">
+                                <span class="speaker-sociales__ocultar">Facebook</span>
+                            </a> 
+                        <?php } ?>
+
+                        <?php if(!empty($redes->twitter)) { ?>
+                            <a class="speaker-sociales__enlace" rel="noopener noreferrer" target="_blank" href="<?php echo $redes->twitter; ?>">
+                                <span class="speaker-sociales__ocultar">Twitter</span>
+                            </a> 
+                        <?php } ?> 
+
+                        <?php if(!empty($redes->youtube)) { ?>
+                            <a class="speaker-sociales__enlace" rel="noopener noreferrer" target="_blank" href="<?php echo $redes->youtube; ?>">
+                                <span class="speaker-sociales__ocultar">YouTube</span>
+                            </a> 
+                        <?php } ?> 
+
+                        <?php if(!empty($redes->instagram)) { ?>
+                            <a class="speaker-sociales__enlace" rel="noopener noreferrer" target="_blank" href="<?php echo $redes->instagram; ?>">
+                                <span class="speaker-sociales__ocultar">Instagram</span>
+                            </a> 
+                        <?php } ?> 
+
+                        <?php if(!empty($redes->tiktok)) { ?>
+                            <a class="speaker-sociales__enlace" rel="noopener noreferrer" target="_blank" href="<?php echo $redes->tiktok; ?>">
+                                <span class="speaker-sociales__ocultar">Tiktok</span>
+                            </a> 
+                        <?php } ?> 
+
+                        <?php if(!empty($redes->github)) { ?>
+                            <a class="speaker-sociales__enlace" rel="noopener noreferrer" target="_blank" href="<?php echo $redes->github; ?>">
+                                <span class="speaker-sociales__ocultar">Github</span>
+                            </a>
+                        <?php } ?> 
+                    </nav>
+
+                    <ul class="speaker__listado-skills">
+                        <?php 
+                            $tags = explode(',', $ponente->tags);
+                            foreach($tags as $tag) { 
+                        ?>
+                            <li class="speaker__skill"><?php echo $tag; ?></li>
+                        <?php } ?>
+                    </ul>
+                </div>
+            </div>
+        <?php } ?>
+    </div>
+</section>
+
