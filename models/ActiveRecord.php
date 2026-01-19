2a0b1ffe4159f58525ff3988a5c2c1ff90a47bd6
Mateo Pavoni <mateopavoni905@gmail.com>
2026-01-19 17:48:23 -0300

    feat: agregue logica para poder ordenar los eventos en el backend y mostrarlos en el frontend en el conferencias.php

--- models/ActiveRecord.php
+++ models/ActiveRecord.php
@@ -1,23 +1,17 @@
 <?php
-
 namespace Model;
 
 class ActiveRecord {
 
-    // Base de Datos
     protected static $db;
     protected static $tabla = '';
     protected static $columnasDB = [];
-
-    // Alertas
     protected static $alertas = [];
 
-    // Setear DB
     public static function setDB($database) {
         self::$db = $database;
     }
 
-    // Alertas
     public static function setAlerta($tipo, $mensaje) {
         static::$alertas[$tipo][] = $mensaje;
     }
@@ -31,14 +25,11 @@ class ActiveRecord {
         return static::$alertas;
     }
 
-    // ===============================
-    // CONSULTAS
-    // ===============================
     public static function consultarSQL($query) {
         $resultado = self::$db->query($query);
         $array = [];
 
-        while ($registro = $resultado->fetch_assoc()) {
+        while($registro = $resultado->fetch_assoc()) {
             $array[] = static::crearObjeto($registro);
         }
 
@@ -49,21 +40,18 @@ class ActiveRecord {
     protected static function crearObjeto($registro) {
         $objeto = new static;
 
-        foreach ($registro as $key => $value) {
-            if (property_exists($objeto, $key)) {
+        foreach($registro as $key => $value ) {
+            if(property_exists($objeto, $key)) {
                 $objeto->$key = $value;
             }
         }
         return $objeto;
     }
 
-    // ===============================
-    // ATRIBUTOS
-    // ===============================
     public function atributos() {
         $atributos = [];
-        foreach (static::$columnasDB as $columna) {
-            if ($columna === 'id') continue;
+        foreach(static::$columnasDB as $columna) {
+            if($columna === 'id') continue;
             $atributos[$columna] = $this->$columna;
         }
         return $atributos;
@@ -73,75 +61,73 @@ class ActiveRecord {
         $atributos = $this->atributos();
         $sanitizado = [];
 
-        foreach ($atributos as $key => $value) {
+        foreach($atributos as $key => $value ) {
             $sanitizado[$key] = self::$db->escape_string($value);
         }
-
         return $sanitizado;
     }
 
-    public function sincronizar($args = []) {
-        foreach ($args as $key => $value) {
-            if (property_exists($this, $key) && $value !== null) {
+    public function sincronizar($args = []) { 
+        foreach($args as $key => $value) {
+            if(property_exists($this, $key) && !is_null($value)) {
                 $this->$key = $value;
             }
         }
     }
 
-    // ===============================
-    // CRUD
-    // ===============================
     public function guardar() {
         return !is_null($this->id) ? $this->actualizar() : $this->crear();
     }
 
     public static function all($orden = 'DESC') {
-        $query = "SELECT * FROM " . static::$tabla . " ORDER BY id {$orden}";
+        $query = "SELECT * FROM " . static::$tabla . " ORDER BY id " . $orden;
         return self::consultarSQL($query);
     }
 
     public static function find($id) {
-        $id = self::$db->escape_string($id);
-        $query = "SELECT * FROM " . static::$tabla . " WHERE id = {$id}";
+        $query = "SELECT * FROM " . static::$tabla . " WHERE id = " . $id;
         $resultado = self::consultarSQL($query);
         return array_shift($resultado);
     }
 
     public static function get($limite) {
-        $query = "SELECT * FROM " . static::$tabla . " ORDER BY id DESC LIMIT {$limite}";
+        $query = "SELECT * FROM " . static::$tabla . " ORDER BY id DESC LIMIT " . $limite;
         return self::consultarSQL($query);
     }
 
     public static function paginar($por_pagina, $offset) {
-        $query = "SELECT * FROM " . static::$tabla . " ORDER BY id DESC LIMIT {$por_pagina} OFFSET {$offset}";
+        $query = "SELECT * FROM " . static::$tabla .
+                 " ORDER BY id DESC LIMIT " . $por_pagina .
+                 " OFFSET " . $offset;
         return self::consultarSQL($query);
     }
 
     public static function where($columna, $valor) {
-        $valor = self::$db->escape_string($valor);
-        $query = "SELECT * FROM " . static::$tabla . " WHERE {$columna} = '{$valor}'";
+        $query = "SELECT * FROM " . static::$tabla .
+                 " WHERE " . $columna . " = '" . $valor . "'";
         $resultado = self::consultarSQL($query);
         return array_shift($resultado);
     }
 
     public static function ordenar($columna, $orden) {
-        $query = "SELECT * FROM " . static::$tabla . " ORDER BY {$columna} {$orden}";
+        $query = "SELECT * FROM " . static::$tabla .
+                 " ORDER BY " . $columna . " " . $orden;
         return self::consultarSQL($query);
     }
 
     public static function ordenarLimite($columna, $orden, $limite) {
-        $query = "SELECT * FROM " . static::$tabla . " ORDER BY {$columna} {$orden} LIMIT {$limite}";
+        $query = "SELECT * FROM " . static::$tabla .
+                 " ORDER BY " . $columna . " " . $orden .
+                 " LIMIT " . $limite;
         return self::consultarSQL($query);
     }
 
     public static function whereArray($array = []) {
         $query = "SELECT * FROM " . static::$tabla . " WHERE ";
 
-        foreach ($array as $key => $value) {
-            $value = self::$db->escape_string($value);
-            $query .= "{$key} = '{$value}'";
-
-            if ($key !== array_key_last($array)) {
+        foreach($array as $key => $value) {
+            $query .= $key . " = '" . $value . "'";
+            if($key !== array_key_last($array)) {
                 $query .= " AND ";
             }
         }
@@ -149,15 +135,11 @@ class ActiveRecord {
         return self::consultarSQL($query);
     }
 
-    // ===============================
-    // TOTALES
-    // ===============================
     public static function total($columna = '', $valor = '') {
         $query = "SELECT COUNT(*) FROM " . static::$tabla;
 
-        if ($columna) {
-            $valor = self::$db->escape_string($valor);
-            $query .= " WHERE {$columna} = '{$valor}'";
+        if($columna) {
+            $query .= " WHERE " . $columna . " = " . $valor;
         }
 
         $resultado = self::$db->query($query);
@@ -168,11 +150,9 @@ class ActiveRecord {
     public static function totalArray($array = []) {
         $query = "SELECT COUNT(*) FROM " . static::$tabla . " WHERE ";
 
-        foreach ($array as $key => $value) {
-            $value = self::$db->escape_string($value);
-            $query .= "{$key} = '{$value}'";
-
-            if ($key !== array_key_last($array)) {
+        foreach($array as $key => $value) {
+            $query .= $key . " = '" . $value . "'";
+            if($key !== array_key_last($array)) {
                 $query .= " AND ";
             }
         }
@@ -182,9 +162,6 @@ class ActiveRecord {
         return array_shift($total);
     }
 
-    // ===============================
-    // CREATE / UPDATE / DELETE
-    // ===============================
     public function crear() {
         $atributos = $this->sanitizarAtributos();
 
@@ -206,22 +183,22 @@ class ActiveRecord {
         $atributos = $this->sanitizarAtributos();
         $valores = [];
 
-        foreach ($atributos as $key => $value) {
-            $valores[] = "{$key}='{$value}'";
+        foreach($atributos as $key => $value) {
+            $valores[] = $key . "='" . $value . "'";
         }
-
-        $id = self::$db->escape_string($this->id);
 
         $query  = "UPDATE " . static::$tabla . " SET ";
         $query .= join(', ', $valores);
-        $query .= " WHERE id = {$id} LIMIT 1";
+        $query .= " WHERE id = '" . self::$db->escape_string($this->id) . "' LIMIT 1";
 
         return self::$db->query($query);
     }
 
     public function eliminar() {
-        $id = self::$db->escape_string($this->id);
-        $query = "DELETE FROM " . static::$tabla . " WHERE id = {$id} LIMIT 1";
+        $query = "DELETE FROM " . static::$tabla .
+                 " WHERE id = " . self::$db->escape_string($this->id) .
+                 " LIMIT 1";
+
         return self::$db->query($query);
     }
 }
