5975e2e642c03d44ddc7a53b53c3e40bdd470ee3
Mateo Pavoni <mateopavoni905@gmail.com>
2026-01-20 00:41:29 -0300

    feat: agregue logica para tener un bloque de los ultimos registrados en el inicio de admin

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -2,12 +2,24 @@
 
 namespace Controllers;
 
+use Model\Evento;
+use Model\Registro;
+use Model\Usuario;
 use MVC\Router;
 
 class DashboardController {
+
     public static function index(Router $router) {
+
+        // Obtener ultimos registros
+        $registros = Registro::get(5);
+        foreach($registros as $registro) {
+            $registro->usuario = Usuario::find($registro->usuario_id);
+        }
+
         $router->render('admin/dashboard/index', [
-            'titulo' => "Panel de Administración"
+            'titulo' => 'Panel de Administración',
+            'registros' => $registros,
         ]);
     }
 }
